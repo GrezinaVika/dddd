50b54c192c49269329b1a808146d27c1c2aed199
Amikor <55d55r77@gmail.com>
2025-09-20 15:18:35 +0000

    add get_product

--- main.py
+++ main.py
@@ -2,6 +2,37 @@ from fastapi import FastAPI
 
 app = FastAPI(title="MyShop", version="0.0.1")
 
-@app.get("/")
+shop_db ={
+    0 : {
+        "name" : "Шампунь",
+        "quantity": 3,
+        "price" : 100
+    },
+    1 : {
+        "name" : "Пицца",
+        "quantity": 30,
+        "price" : 500
+    },
+    2 : {
+        "name" : "Вода",
+        "quantity": 10,
+        "price" : 50
+    },
+}
+
+
+@app.get("/", tags=["Магазин"])
 async def home():
     return {"data" : "Welcome to my Shop"}
+
+@app.get("/products", tags=["Продукты"])
+async def get_produtcs():
+    return {"data" : shop_db}
+
+@app.get("/products/{id}", tags=["Продукты"])
+async def get_produtc(id: int):
+    if shop_db.get(id, None):
+        return {"data" : shop_db[id]}
+    else:
+        return {"msg" : "Товара не существует"}
+
