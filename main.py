02d5c09a2c161032c52279a728497cb373cd8804
Amikor <55d55r77@gmail.com>
2025-09-27 14:44:47 +0000

    add delete product

--- main.py
+++ main.py
@@ -27,33 +27,36 @@ shop_db ={
     },
 }
 
+count:int = 3
 
 @app.get("/", tags=["Магазин"])
 async def home():
     return {"data" : ["Welcome to my Shop"]}
 
 @app.get("/products/", tags=["Продукты"])
-async def get_produtcs():
+async def get_products():
     return {"data" : shop_db}
 
 @app.get("/products/{id}", tags=["Продукты"])
-async def get_produtc(id: int):
+async def get_product(id: int):
     if shop_db.get(id, None):
         return {"data" : shop_db[id]}
     else:
         return {"msg" : "Товара не существует"}
 
 @app.post("/products/", tags=["Продукты"])
-async def add_produtc(data: dict):
-    id = len(shop_db.keys())
+async def add_product(data: dict):
+    global count
+    id = count
     if shop_db.get(id, None):
         return {"msg" : "Такой товар существует"}
     else:
         shop_db[id] = data
+        count += 1
         return {"data" : data}
 
 @app.put("/products/{id}", tags=["Продукты"])
-async def edit_produtc(id: int, data: dict):
+async def edit_product(id: int, data: dict):
     if shop_db.get(id, None):
         shop_db[id] = data
         return {"data" : shop_db[id]}
@@ -62,13 +65,22 @@ async def edit_produtc(id: int, data: dict):
 
 
 @app.patch("/products/{id}", tags=["Продукты"])
-async def edit_partialy_produtc(id: int, data: dict):
+async def edit_partialy_product(id: int, data: dict):
     if shop_db.get(id, None):
         product = shop_db[id]
         for k, v in data.items():
             if product.get(k, None):
                 shop_db[id][k] = v
-                
+
         return {"data" : shop_db[id]}
+    else:
+        return {"msg" : "Товара не существует"}   
+    
+@app.delete("/products/{id}", tags=["Продукты"])
+async def delete_product(id: int):
+    if shop_db.get(id, None):
+        product = shop_db[id]
+        del shop_db[id]        
+        return {"msg" : f"{product} был удален"}
     else:
         return {"msg" : "Товара не существует"}   
